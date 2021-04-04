8c48ed6f61083a3a0dd7413e21d117ef7a64ced7
calumpetergunn <calumgunn@gmail.com>
2021-04-04 13:33:14 +0100

    able to add new items and delete all

--- js/app.js
+++ js/app.js
@@ -14,4 +14,28 @@ const handleNewItemFormSubmit = (event) => {
     birdList.appendChild(birdListItem)
 
     event.target.reset();
-} 
+};
+
+const createBirdListItem = (form) => {
+    const birdListItem = document.createElement('li');
+    birdListItem.classList.add('bird-list-item');
+
+    const name = document.createElement('h2');
+    name.textContent = form.name.value;
+    birdListItem.appendChild(name);
+
+    const species = document.createElement('h3');
+    species.textContent = form.species.value;
+    birdListItem.appendChild(species);
+
+    const status = document.createElement('h3');
+    status.textContent = form.status.value;
+    birdListItem.appendChild(status);
+
+    return birdListItem;
+}
+
+const handleDeleteAllClick = (event) => {
+    const birdList = document.querySelector('#native-bird');
+    birdList.innerHTML = '';
+}
